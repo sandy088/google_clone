f70a84cebb4eb92b5c87e7facf035e7fd42a2c0b
Sandeep <sandeepsandy0880@gmail.com>
2022-07-13 13:19:12 +0530

    Project Day-> Web UI Created

---
+++ lib/widgets/search.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_clone/colors.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            "assets/images/google-logo.png",
+            height: size.height * 0.12,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          width: size.width * 0.4,
+          child: TextFormField(
+            decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: searchBorder,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(28))),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(
+                    'assets/images/search-icon.svg',
+                    color: searchBorder,
+                  ),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(
+                    'assets/images/mic-icon.svg',
+                  ),
+                )),
+          ),
+        )
+      ],
+    );
+  }
+}
