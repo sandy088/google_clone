7e6e2023967133c696af75819562eb513c3c23c4
Sandeep <sandeepsandy0880@gmail.com>
2022-07-21 12:12:05 +0530

    Added Search results

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_clone/colors.dart';
+import 'package:google_clone/services/api_services.dart';
 import 'package:google_clone/widgets/search_footer.dart';
 import 'package:google_clone/widgets/search_tabs.dart';
 import 'package:google_clone/widgets/web/search_header.dart';
+import 'package:google_clone/widgets/web/search_result_component.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
@@ -27,7 +30,57 @@ class SearchScreen extends StatelessWidget {
             height: 0,
             thickness: 1,
           ),
+
+
+
           //search results
+          FutureBuilder(
+            future: ApiService().fetchData(queryTerm: "weboking"),
+            
+            builder: (BuildContext context, AsyncSnapshot snapshot ){
+              if(snapshot.hasData){
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(left: 150, top: 12),
+                      child:  Text("About ${snapshot.data?['searchInformation']['formattedTotalResults']} results (${snapshot.data?['searchInformation']['formattedSearchTime']} seconds)",
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Color(0xFF70757a),),
+                    ),
+                    ),
+                    ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: snapshot.data?['items'].length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: EdgeInsets.only(
+                                    left: size.width <= 768 ? 10 : 150,
+                                    top: 10),
+                                child: SearchResultCompnent(
+                                  linkToGo: snapshot.data?['items'][index]
+                                      ['link'],
+                                  link: snapshot.data?['items'][index]
+                                      ['formattedUrl'],
+                                  text: snapshot.data?['items'][index]['title'],
+                                  desc: snapshot.data?['items'][index]
+                                      ['snippet'],
+                                ),
+                              );
+                            },
+                          ),
+                  ],
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            ),
+
 
           //pagination
 
